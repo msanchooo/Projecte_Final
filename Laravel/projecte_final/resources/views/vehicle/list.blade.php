5bf59f0e846965424240014cebef961ea3c6b6af
msancho <marc_sanchopiedehierro@iescarlesvallbona.cat>
2023-03-28 17:31:55 +0200

    view vehicles
    Co-authored-by: Albert Perez <albertprz1@users.noreply.github.com>

--- Laravel/projecte_final/resources/views/vehicle/list.blade.php
+++ Laravel/projecte_final/resources/views/vehicle/list.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
     <h1>Llistat dde vehicles</h1>
-    <a href="{{ route('vehicle_new') }}">+ Nou vehicle</a>
+    <a href="{{ route('vehicle_insert') }}">+ Nou vehicle</a>
 
     @if (session('status'))
         <div>
@@ -19,15 +19,21 @@
     <table style="margin-top: 20px;margin-bottom: 10px;">
         <thead>
             <tr>
-                <th>Matricula</th><th>Marca</th><th>Model</th><th>Tipus</th>
+                <th>Matricula</th><th>Marca</th><th>Model</th><th>Tipus</th><th>Client</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($vehicles as $vehicle)
                 <tr>
-                    <td>{{ $vehicle->matricula }}</td><td>{{ $vehicle->marca }}</td><td>{{ $vehicle->model }}</td><td>{{ $vehicle->tipus }}</td>
+                    <td>{{ $vehicle->matricula }}</td>
+                    <td>{{ $vehicle->marca }}</td>
+                    <td>{{ $vehicle->model }}</td>
+                    <td>{{ $vehicle->tipus }}</td>
+
+                    <!-- hacer la funcion coger nombre cliente asociado al vehiculo en el modelo -->
+                    <td>{{ $vehicle->getNomClient() }}</td>
                     <td>
-                        <a href="{{ route('vehicle_edit', ['id' => $vehicle->id]) }}">Editar</a>
+                        <a href="{{ route('vehicle_update', ['id' => $vehicle->id]) }}">Editar</a>
                         <a href="{{ route('vehicle_delete', ['id' => $vehicle->id]) }}">Eliminar</a>
                     </td>
                 </tr>
--- Laravel/projecte_final/resources/views/vehicle/new.blade.php
+++ Laravel/projecte_final/resources/views/vehicle/new.blade.php
@@ -1,31 +1,29 @@
 @extends('layout')
 
-@section('title', 'Editar Vehicle')
+@section('title', 'Nou vehicle')
 
 @section('stylesheets')
     @parent
 @endsection
 
 @section('content')
-    <h1>Editar Vehicle</h1>
+    <h1>Nou Vehicle</h1>
     <a href="{{ route('vehicle_list') }}">&laquo; Torna</a>
 	<div style="margin-top: 20px">
-        <form method="POST" action="{{ route('vehicle_update', ['id' => $vehicle->id]) }}">
+        <form method="POST" action="{{ route('vehicle_insert') }}">
             @csrf
             <div>
                 <label for="matricula">Matricula</label>
-                <input type="text" name="matricula" value="{{ $vehicle->matricula }}" />
+                <input type="text" name="matricula" />
             </div>
             <div>            
                 <label for="marca">Marca</label>
-                <input type="text" name="marca" value="{{ $vehicle->marca }}" />
+                <input type="text" name="marca"  />
             </div>
             <div>
                 <label for="model">Model</label>
-                <input type="text" name="model" value="{{ $vehicle->model }}" />
+                <input type="text" name="model"  />
             </div>
-
-            <!-- hacer el selected -->
             <div>
                 <label for="tipus">Tipus</label>
                 <select name="tipus">
@@ -33,8 +31,6 @@
                     <option value="2">Moto</option>
                 </select>
             </div>
-
-            <!-- hacer selected cliente -->
             <div>
                 <label for="client_id">Client</label>
                 <select name="client_id">
