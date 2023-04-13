97f547cfb50a56151232e9fed95b448c41542e0d
albertprz1 <albeertp55@gmail.com>
2023-04-13 16:01:30 +0200

    Login/logout

--- Angular/projecte_angular/src/app/nav-bar/nav-bar.component.ts
+++ Angular/projecte_angular/src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  constructor(public auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigate(['/body']);
+      }
+    });
+  }
+
+  login() {
+    this.auth.loginWithRedirect();
+  }
+
+  logOut(){
+    this.auth.logout()
+  }
 
 }
+
