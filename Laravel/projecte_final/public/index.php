8f84860f527fcc28eba0e1bc7c040221974f06bd
albertprz1 <albeertp55@gmail.com>
2023-05-24 19:37:46 +0200

    Form to Mail

--- Laravel/projecte_final/public/index.php
+++ Laravel/projecte_final/public/index.php
@@ -5,6 +5,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Headers: *');
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
@@ -53,3 +57,5 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
+
+
